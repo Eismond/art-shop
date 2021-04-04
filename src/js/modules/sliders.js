6e55db686ccc4b0a1f41f5793c7c145127fb482f
Eismond <a.eismond@gmail.com>
2021-04-04 18:41:36 +0300

    basic slider

---
+++ src/js/modules/sliders.js
@@ -0,0 +1,49 @@
+const sliders = (slideSelector, direction, prev, next) => {
+    let slideIndex = 1;
+
+    const items = document.querySelectorAll(slideSelector);
+
+    function showSlides(n) {
+        if (n > items.length) {
+            slideIndex = 1;
+        }
+
+        if (n < 1) {
+            slideIndex = items.length;
+        }
+
+        items.forEach(item => {
+            item.classList.add('animated');
+            item.style.display = 'none';
+        })
+
+        items[slideIndex - 1].style.display = 'block';
+    }
+
+    showSlides(slideIndex);
+
+    function changeSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    try {
+        const prevBtn = document.querySelector(prev),
+            nextBtn = document.querySelector(next);
+
+        prevBtn.addEventListener('click', () => {
+            changeSlides(-1);
+            items[slideIndex - 1].classList.remove('slideInLeft');
+            items[slideIndex - 1].classList.add('slideInRight')
+        })
+
+        nextBtn.addEventListener('click', () => {
+            changeSlides(-1);
+            items[slideIndex - 1].classList.remove('slideInRight');
+            items[slideIndex - 1].classList.add('slideInLeft')
+        })
+    } catch (e) {
+
+    }
+}
+
+export default sliders;
