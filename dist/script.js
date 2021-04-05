5af02cfa66895126587c3f786820766394085d0d
Eismond <a.eismond@gmail.com>
2021-04-05 21:20:48 +0300

    turning on accrordion with styles

--- dist/script.js
+++ dist/script.js
@@ -3759,28 +3759,31 @@ __webpack_require__.r(__webpack_exports__);
 
 var accordion = function accordion(triggersSelector, itemsSelector) {
   var btns = document.querySelectorAll(triggersSelector),
-      blocks = document.querySelectorAll(itemsSelector);
-  btns.forEach(function (item) {
-    item.addEventListener('click', function () {
-      this.classList.toggle('active-style');
-      this.nextElementSibling.classList.toggle('active-content');
-
-      if (this.classList.contains('active-style')) {} else {}
-    });
-  }); // blocks.forEach(block => {
-  //     block.classList.add('animated', 'fadeInDown');
-  // })
-  // btns.forEach((btn) => {
-  //     btn.addEventListener('click', function () {
-  //         if (!this.classList.contains('active')) {
-  //             btn.classList.remove('active', 'active-style');
-  //             btns.forEach((btn) => {
-  //                 btn.classList.remove('active', 'active-style');
-  //             });
+      blocks = document.querySelectorAll(itemsSelector); // btns.forEach((item) => {
+  //     item.addEventListener('click', function () {
+  //         this.classList.toggle('active-style');
+  //         this.nextElementSibling.classList.toggle('active-content');
+  //         if (this.classList.contains('active-style')) {
+  //         } else {
   //         }
-  //         this.classList.add('active', 'active-style');
   //     })
   // })
+  // blocks.forEach(block => {
+  //     block.classList.add('animated', 'fadeInDown');
+  // })
+
+  btns.forEach(function (btn) {
+    btn.addEventListener('click', function () {
+      if (!this.classList.contains('active')) {
+        btn.classList.remove('active', 'active-style');
+        btns.forEach(function (btn) {
+          btn.classList.remove('active', 'active-style');
+        });
+      }
+
+      this.classList.add('active', 'active-style');
+    });
+  });
 };
 
 /* harmony default export */ __webpack_exports__["default"] = (accordion);
--- src/js/modules/accordion.js
+++ src/js/modules/accordion.js
@@ -2,35 +2,35 @@ const accordion = (triggersSelector, itemsSelector) => {
     const btns = document.querySelectorAll(triggersSelector),
         blocks = document.querySelectorAll(itemsSelector);
 
-    btns.forEach((item) => {
-        item.addEventListener('click', function () {
-            this.classList.toggle('active-style');
-            this.nextElementSibling.classList.toggle('active-content');
+    // btns.forEach((item) => {
+    //     item.addEventListener('click', function () {
+    //         this.classList.toggle('active-style');
+    //         this.nextElementSibling.classList.toggle('active-content');
 
-            if (this.classList.contains('active-style')) {
+    //         if (this.classList.contains('active-style')) {
 
-            } else {
+    //         } else {
 
-            }
-        })
-    })
+    //         }
+    //     })
+    // })
 
     // blocks.forEach(block => {
     //     block.classList.add('animated', 'fadeInDown');
     // })
 
-    // btns.forEach((btn) => {
-    //     btn.addEventListener('click', function () {
-    //         if (!this.classList.contains('active')) {
-    //             btn.classList.remove('active', 'active-style');
-    //             btns.forEach((btn) => {
-    //                 btn.classList.remove('active', 'active-style');
-    //             });
-    //         }
+    btns.forEach((btn) => {
+        btn.addEventListener('click', function () {
+            if (!this.classList.contains('active')) {
+                btn.classList.remove('active', 'active-style');
+                btns.forEach((btn) => {
+                    btn.classList.remove('active', 'active-style');
+                });
+            }
 
-    //         this.classList.add('active', 'active-style');
-    //     })
-    // })
+            this.classList.add('active', 'active-style');
+        })
+    })
 }
 
 export default accordion;
